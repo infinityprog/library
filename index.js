385b2b3e8cd2aae1aaef4fd837ddb5e0bd14ebe8
infinityprog <hugo220798@gmail.com>
2020-10-18 12:24:53 +0200

    heroku conf

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PassageService = require('./business/PassageService');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const passageService = new PassageService();
 
 let incr = 0;
