09a13eb4c31e6ad917f7b9b6d50778656cfedc45
ironcraft98 <ironcraft98.louis@gmail.com>
2020-10-18 12:39:50 +0200

    refactoring

--- index.js
+++ index.js
@@ -6,19 +6,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const passageService = new PassageService();
 
-let incr = 0;
-
 app.get('/passage/more', async (req, res) => {
     res.json(await passageService.more());
 });
 
 app.get('/passage', async (req, res) => {
     res.json(await passageService.findAll());
-});
-
-app.get('/reset', (req, res) => {
-    incr = 0;
-    res.json(incr);
 });
 
 app.listen(port, () => console.log(`run library api on port ${port}!`))
--- persistance/dao/PassageDao.js
+++ persistance/dao/PassageDao.js
@@ -2,6 +2,11 @@ const {connection} = require("../../db/connection").module;
 
 class PassageDao {
 
+    /**
+     * <h1>Find data of the day<h1>
+     *
+     * @returns {Promise<Passage>}
+     */
     async findNow() {
         return new Promise(resolve => {
             connection.query('SELECT * FROM passage where day = $1', [new Date().toLocaleDateString()], (error, result) => {
@@ -19,14 +24,23 @@ class PassageDao {
         })
     }
 
+    /**
+     * <h1>Insert data today<h1>
+     */
     insert() {
         connection.query('insert into passage (day, number) values ($1, $2)', [new Date().toLocaleDateString(), 1]);
     }
 
+    /**
+     * <h1>update data today with number<h1>
+     */
     update(number) {
         connection.query('update passage set number = $1 where day = $2', [number, new Date().toLocaleDateString()])
     }
 
+    /**
+     * <h1>Find all passage<h1>
+     */
     findAll() {
         return new Promise(resolve => {
             connection.query('SELECT * FROM passage', (error, result) => {
